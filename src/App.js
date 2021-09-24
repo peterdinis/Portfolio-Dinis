d29b0cf89773c66410b93bbc7b04b9efab7d7eeb
Peter Dinis <peterdinis0@pobox.sk>
2021-09-24 13:21:06 +0200

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from './components/shared/Navbar';
+import Errorpage from "./pages/Errorpage";
 import Homepage from "./pages/Homepage";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path='/404' component={Errorpage} />
+        <Redirect from='*' to='/404' />
       </Switch>
     </div>
   );
