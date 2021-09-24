93f0419fa84f77d856e4a131710a07d6b746dbdd
Peter Dinis <peterdinis0@pobox.sk>
2021-09-24 11:24:34 +0200

    Edit some news features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Switch, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
