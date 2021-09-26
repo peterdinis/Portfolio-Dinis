aaf77cb797849ef4ef3a45ebc687145d152dbb7a
Peter Dinis <peterdinis0@pobox.sk>
2021-09-26 17:12:09 +0200

    Setup translation and re-edit modal

--- src/components/projects/Knowledge.jsx
+++ src/components/projects/Knowledge.jsx
@@ -1,37 +1,36 @@
-// import { useState } from "react";
-// import { Paper, Box, Typography } from "@material-ui/core";
+import { useState } from "react";
+import { Paper, Box} from "@material-ui/core";
 import { BorderLinearProgress, useStyles } from "./styles";
 import Skill from "./Skill";
-// import Modal from "@material-ui/core/Modal";
-// import Backdrop from "@material-ui/core/Backdrop";
-// import Fade from "@material-ui/core/Fade";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 import "./Knowledge.css";
-import { Paper } from "@material-ui/core";
 
-// const style = {
-//   position: 'absolute',
-//   top: '50%',
-//   left: '50%',
-//   transform: 'translate(-50%, -50%)',
-//   width: 400,
-//   bgcolor: 'background.paper',
-//   border: '2px solid #000',
-//   boxShadow: 24,
-//   p: 4,
-// };
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Knowledge() {
   const classes = useStyles();
 
-//   const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-//   const handleOpen = () => {
-//     setOpen(true);
-//   };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-//   const handleClose = () => {
-//     setOpen(false);
-//   };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className="pb-2">
@@ -40,11 +39,11 @@ export default function Knowledge() {
           <h1 className="text-center text-4xl text-black">
             Moje znalosti
           </h1>
-          {/* <div className="buttonWrapper">
+          <div className="buttonWrapper">
             <button className="border border-green-500 bg-green-500 text-2xl text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline" onClick={handleOpen}>Info</button>
-          </div> */}
+          </div>
 
-          {/* <Modal
+         <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -57,15 +56,13 @@ export default function Knowledge() {
       >
         <Fade in={open}>
           <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <p className="infoText">
+              Mierne pokročilý - Pre-intermediate, <br />
+              Učím sa: I'm learning
+            </p>
           </Box>
         </Fade>
-      </Modal> */}
+      </Modal>
           <div>
             <Skill name="Python" status="Mierne pokročilý" />
             <BorderLinearProgress variant="determinate" value={65} />
