634868ed57c9abe3067ac6b737ea7ed9bd252f0f
Peter Dinis <peterdinis0@pobox.sk>
2021-09-26 17:16:06 +0200

    Small updates

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <>
       <div className="w-full  bg-center bg-no-repeat bg-cover main">
         <div className="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
-          <div className="mx-4 text-center text-white mb-12">
+          <div className="mx-4 text-center shadow-lg text-white mb-12">
             <h1 className="font-bold text-8xl mb-4">Peter Dinis</h1>
             <h2 className="font-bold text-4xl mb-12">
               Junior FullStack Developer
--- src/components/projects/Knowledge.jsx
+++ src/components/projects/Knowledge.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Paper, Box} from "@material-ui/core";
+import { Paper, Box } from "@material-ui/core";
 import { BorderLinearProgress, useStyles } from "./styles";
 import Skill from "./Skill";
 import Modal from "@material-ui/core/Modal";
@@ -8,13 +8,13 @@ import Fade from "@material-ui/core/Fade";
 import "./Knowledge.css";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -36,33 +36,36 @@ export default function Knowledge() {
     <div className="pb-2">
       <div className={classes.root}>
         <Paper className="mt-2 pt-2" elevation={3}>
-          <h1 className="text-center text-4xl text-black">
-            Moje znalosti
-          </h1>
+          <h1 className="text-center text-4xl text-black">Moje znalosti</h1>
           <div className="buttonWrapper">
-            <button className="border border-green-500 bg-green-500 text-2xl text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline" onClick={handleOpen}>Info</button>
+            <button
+              className="border border-green-500 bg-green-500 text-2xl text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline"
+              onClick={handleOpen}
+            >
+              Info
+            </button>
           </div>
 
-         <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <p className="infoText">
-              Mierne pokročilý - Pre-intermediate, <br />
-              Učím sa: I'm learning
-            </p>
-          </Box>
-        </Fade>
-      </Modal>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <Box sx={style}>
+                <p className="infoText">
+                  Mierne pokročilý - Pre-intermediate, <br />
+                  Učím sa: I'm learning
+                </p>
+              </Box>
+            </Fade>
+          </Modal>
           <div>
             <Skill name="Python" status="Mierne pokročilý" />
             <BorderLinearProgress variant="determinate" value={65} />
