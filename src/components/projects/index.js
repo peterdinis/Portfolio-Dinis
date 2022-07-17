25c62db0c353f245c2495298f7c5d10c1bd10be4
Peterdinis <peterdinis0@pobox.sk>
2022-07-17 18:10:55 +0200

    Finish basic index files

---
+++ src/components/projects/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Knowledge = React.lazy(() =>import("./Knowledge/Knowledge"));
+export const KnowledgeWrapper = React.lazy(() =>import("./Knowledge/KnowledgeWrapper"));
+export const MyProjects = React.lazy(() =>import("./Projects/MyProjects"));
+export const Project = React.lazy(() =>import("./Project/Project"));
+export const Skill = React.lazy(() =>import("./Skill/Skill"));
+export const Timeline = React.lazy(() =>import("./Timeline/TimeLine"));
---
+++ src/components/shared/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const AppFooter = React.lazy(() => import("./AppFooter"));
+export const Header = React.lazy(() => import("./Header"));
+export const HelperModal = React.lazy(() => import("./HelperModal"));
+export const Navbar = React.lazy(() => import("./Navbar"));
