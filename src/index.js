3fa3a8405622a111d26a7ff36597ba5b59675869
Peterdinis <peterdinis0@pobox.sk>
2022-07-17 18:25:21 +0200

    WIP Start to updating packages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import "./i18n/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
