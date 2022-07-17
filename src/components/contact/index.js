20b3ddfa281d121bb7283c85ff32756d50c9d3ca
Peterdinis <peterdinis0@pobox.sk>
2022-07-17 18:07:48 +0200

    Adding helper index files

---
+++ src/components/contact/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ContactInfo = React.lazy(() =>import("./ContractInfo/ContactInfo"));
+export const Link = React.lazy(() =>import("./Link/Links"));
+export const Mailto  = React.lazy(() =>import("./Mailto/Mailto"));
---
+++ src/components/home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Hero = React.lazy(() => import("./Hero/Hero"));
+export const Images = React.lazy(() => import("./Images/Images"));
+export const Info = React.lazy(() => import("./Info/Info"));
