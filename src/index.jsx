4051c25d6adea4f15f4e9242d246b996cd7d4bed
Peterdinis <peterdinis0@pobox.sk>
2022-08-06 10:11:04 +0200

    Making small changes

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n/i18n";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 root.render(
   <StrictMode>
-      <App />
-  </StrictMode>,
-  document.getElementById("root")
+    <App />
+  </StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
