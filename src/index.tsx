5a3d44f2f4f35adcaf23c1da50cf60fe46c594d5
Peterdinis <peterdinis0@pobox.sk>
2022-08-09 20:21:01 +0200

    Fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React, {StrictMode} from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 import "./i18n/i18n";
 
-const root = ReactDOM.createRoot(document.getElementById("root")) as HTMLElement;
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <StrictMode>
       <App />
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
