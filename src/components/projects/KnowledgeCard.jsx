4babe63d294ed4d2f48939e73c95c288528db23b
Peterdinis <peterdinis0@pobox.sk>
2022-08-22 22:25:32 +0200

    Refactoring knowledge part in portfolio

---
+++ src/components/projects/KnowledgeCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function KnowledgeCard(props) {
+  return (
+    <div className="bg-white dark:bg-gray-800 rounded shadow px-8 py-6 flex items-center">
+      <div className="p-4 rounded">
+        <img
+          className="w-20"
+          src={props.image}
+        />
+      </div>
+      <div className="ml-6">
+        <h3 className="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-bold text-2xl">
+          {props.name}
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default KnowledgeCard;
