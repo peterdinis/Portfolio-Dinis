75ad617121357829650a26fa2f9a262aa04e4e43
Peterdinis <peterdinis0@pobox.sk>
2022-08-24 17:38:45 +0200

    Small change in Hero component

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <>
       <div className="w-full  bg-center bg-no-repeat bg-cover main">
         <div className="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
-          <div className="mx-4 text-center shadow-inner opacity-90 text-white mb-36">
+          <div className="mx-4 text-center text-white mb-36">
             <h1 className="font-bold text-8xl mb-4">Peter Dinis</h1>
             <h2 className="font-bold text-4xl mb-12">
               Junior FullStack developer
