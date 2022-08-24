40d3dd45592a47ce8324fea094b55cbaab22888e
Peterdinis <peterdinis0@pobox.sk>
2022-08-24 18:10:45 +0200

    Updating knowledge wrapper component

--- src/components/projects/KnowledgeWrapper.jsx
+++ src/components/projects/KnowledgeWrapper.jsx
@@ -75,7 +75,7 @@ function KnowledgeWrapper() {
             name="React-Query"
           />
           <KnowledgeCard
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+            image={javascriptLogo}
             name="Javascript"
           />
           <KnowledgeCard
