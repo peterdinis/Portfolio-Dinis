09cf049116b3afcdfa69113c4bb10fddc1f23980
Peterdinis <peterdinis0@pobox.sk>
2022-10-01 15:36:39 +0200

    foo

--- src/components/projects/MyProjects.jsx
+++ src/components/projects/MyProjects.jsx
@@ -36,16 +36,8 @@ const MyProjects = () => {
         <Project
           name={t("description.foodApp")}
           description={t("description.foodClient")}
-          subName="React Typescript React-Query "
-          link="https://github.com/peterdinis/React-Shop"
-          firstLinkName="Github"
-        />
-
-        <Project
-          name={t("description.foodApp")}
-          description={t("description.foodAPI")}
-          subName="Python Django Postgresql"
-          link="https://github.com/peterdinis/Django-Shop-API"
+          subName="React Typescript Firebase"
+          link="https://github.com/peterdinis/React-Firebase-Shop"
           firstLinkName="Github"
         />
 
