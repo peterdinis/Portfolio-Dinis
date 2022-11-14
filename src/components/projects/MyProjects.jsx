aead1afb00481690d7b18b2a8fca7fb88427f1fc
Peterdinis <peterdinis0@pobox.sk>
2022-11-14 18:57:28 +0100

    Updating projects

--- src/components/projects/MyProjects.jsx
+++ src/components/projects/MyProjects.jsx
@@ -9,8 +9,7 @@ const MyProjects = () => {
         {t("description.myProjects")}
       </h1>
       <div className="mt-6 px-10 grid grid-cols-4 gap-4">
-        
-      <Project 
+        <Project
           name={t("description.projectOne")}
           description={t("description.projectOneD")}
           subName="React Typescript React Query Tailwind"
@@ -18,14 +17,13 @@ const MyProjects = () => {
           firstLinkName="Github"
         />
 
-<Project
+        <Project
           name={t("description.projectTwo")}
           description={t("description.projectTwoD")}
           subName="Nestjs Postgresql"
           link="https://github.com/peterdinis/Spst-Kniznica-Server"
           firstLinkName="Github"
         />
-
 
         <Project
           name="Node Typescript Template"
@@ -43,10 +41,18 @@ const MyProjects = () => {
           firstLinkName="Github"
         />
 
-        <Project 
+        <Project
           name={t("description.insta")}
           description={t("description.insta")}
-          subName="Nx React Node Typescript Mongodb"
+          subName="React Typescript Redux Toolkit"
+          link="https://github.com/peterdinis/Devstagram"
+          firstLinkName="Github"
+        />
+
+        <Project
+          name={t("description.instaBE")}
+          description={t("description.instaBE")}
+          subName="Node Typescript Mongodb Jest"
           link="https://github.com/peterdinis/Devstagram"
           firstLinkName="Github"
         />
