f029f87c525f9beb2116db424c7f97c4dbfd2506
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 20:35:47 +0100

    feat: update MyProjects

--- src/components/projects/MyProjects.jsx
+++ src/components/projects/MyProjects.jsx
@@ -11,8 +11,15 @@ const MyProjects = () => {
       <div className="mt-6 px-10 grid grid-cols-4 gap-4">
         <Project
           name={t("description.projectOne")}
-          subName="Nextjs Prisma Postgresql"
+          subName="Nextjs React-Query Typescript"
           link="https://github.com/peterdinis/Spst-Kniznica"
+          firstLinkName="Github"
+        />
+
+        <Project
+          name={t("description.projectTwo")}
+          subName="NodeJs Express Typescript Prisma"
+          link="https://github.com/peterdinis/Spst-Kniznica-API"
           firstLinkName="Github"
         />
 
@@ -53,6 +60,13 @@ const MyProjects = () => {
           name={t("description.foo")}
           subName="React Typescript Tailwind"
           link="https://github.com/peterdinis/React-Eshop"
+          firstLinkName="Github"
+        />
+
+        <Project
+          name={t("description.foodAPI")}
+          subName="Node Prisma Typescript Express"
+          link="https://github.com/peterdinis?tab=repositories"
           firstLinkName="Github"
         />
 
